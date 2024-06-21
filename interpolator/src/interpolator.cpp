c83bd8acd02386c737a683f37d5abe030d35daca
Anderko Máté <041anderko.mate@gmail.com>
2024-06-21 21:59:47 +0200

    started working on geotiff module

--- interpolator/src/interpolator.cpp
+++ interpolator/src/interpolator.cpp
@@ -26,25 +26,6 @@ std::map<std::string, LithologyData> interpolate() {
         //writeMatrixCoordinates(data.interpolatedData, data.stratumName);
         //layers.push_back(toPointVector(data.interpolatedData));
     }
-    std::vector<std::string> seq = {"Qh","jT1", "k_c-tP3-T1",  "k_kP3"};//{"talaj","kisberi","csatkai","szoci","rudolfhazi","kisgyoni","ganti"};//
-   for (auto it = lithologyMap.begin(); it != lithologyMap.end(); ++it) {
-        auto& data = it->second;
-        auto place = std::find(seq.begin(), seq.end(), it->first);
-        if (place != seq.end() && place != seq.begin()) {
-            auto prevPlace = std::prev(place);
-            std::string prevKey = *prevPlace;
 
-            if (lithologyMap.find(prevKey) != lithologyMap.end()) {
-                auto& prevData = lithologyMap[prevKey];
-                for (size_t i = 0; i < data.interpolatedData.size(); ++i) {
-                    if (i < prevData.interpolatedData.size()) {
-                        if (prevData.interpolatedData[i].z > data.interpolatedData[i].z) {
-                            data.interpolatedData[i].z = prevData.interpolatedData[i].z;
-                        }
-                    }
-                }
-            }
-        }
-    }
     return lithologyMap;
 }
